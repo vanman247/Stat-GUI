466085f5b3ca35ef1bfc12d7cdbc27af1e85fe75
vanman247 <van15049@byui.edu>
2021-09-17 21:52:38 -0600

    Updating the GUI
    Signed-off-by: vanman247 <van15049@byui.edu>

--- GUI.py
+++ GUI.py
@@ -1,12 +1,23 @@
-import tkinter as tk                    
+import tkinter as tk
 from tkinter import ttk
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog as fd
+import tksheet
+import pandas as pd
+import numpy as np
 import os
   
   
 root = tk.Tk()
 root.title("Statistical Analysis")
+root.geometry('1000x600')
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 1000)
+    
+##style = ttk.Style(root)
+##style.configure('TNotebook.Tab', width=root.winfo_screenwidth())
+
 
 ## SETTING UP THE TABS
 tabControl = ttk.Notebook(root)
@@ -55,43 +66,37 @@ ttk.Label(tab5,
                                     row = 0, 
                                     padx = 30,
                                     pady = 30)
-## IMPORTING DATA TAB
-def show():
-    url = "C:/Users/Ammon Van/Downloads/"
-    tf = filedialog.askopenfilename(initialdir = url,
-                                          title = "Select a File",
-                                          filetypes = (("CSV files",
-                                                        "*.csv*"),
-                                                       ("All files",
-                                                        "*.*")))
-    pathh.insert(END, tf)
-    tf = open(tf)  # or tf = open(tf, 'r')
-    data = tf.read()
-    T.insert(END, data)
-    tf.close()
-    
-pathh = Entry(tab0)
-  
-  
-# Create button, it will change label text
-button = Button(tab0, text = "Import CSV" , command = show).grid(column = 0,
-           row = 0,
-           padx = 5,
-           pady = 5)
-  
-# Create Label
-label = Label(tab0, text = " " )
-label.grid(column = 2,
-           row = 0,
-           padx = 5,
-           pady = 5)
 
 # Create Text Field to display Data
-T = Text(tab0).grid(column = 2,
-           row = 2,
+text = tk.Text(tab0, width=root.winfo_screenwidth(), height=root.winfo_screenheight())
+text.grid(column = 1,
+           row = 1,
            padx = 15,
-           pady = 15)
+           pady = 15,
+           sticky='ew')
 
+
+## IMPORTING DATA TAB
+def open_text_file(): 
+    # file type
+    filetypes = (
+        ('CSV files', '*.csv'),
+        ('All files', '*.*')
+    )
+    # show the open file dialog
+    f = fd.askopenfile(filetypes=filetypes)
+    df = pd.read_csv(f, encoding='utf-8')
+    # read the text file and show its content on the Text
+    text.insert('1.0', df)
+
+open_button = ttk.Button(tab0,
+                         text='Import CSV',
+                         command=open_text_file)
+open_button.grid(column = 0,
+           row = 0,
+           padx = 5,
+           pady = 5)
 
 ## Run the App
+
 root.mainloop()  
